65020e52d40e2c683c34039b5ac7c22f81fa7af7
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-07-09 11:13:59 -0700

    log in functionality

--- djackets_vue/src/App.vue
+++ djackets_vue/src/App.vue
@@ -71,6 +71,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data() {
     return {
@@ -82,6 +83,14 @@ export default {
   },
   beforeCreate() {
     this.$store.commit('initializeStore')
+
+    const token = this.$store.state.token
+
+    if (token) {
+        axios.defaults.headers.common['Authorization'] = "Token " + token
+    } else {
+        axios.defaults.headers.common['Authorization'] = ""
+    }
   },
   mounted() {
     this.cart = this.$store.state.cart
